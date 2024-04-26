db3ea18902daefdfaffe5189252f598a4c7224c9
michael <michaelngige@gmail.com>
2024-04-26 15:15:56 +0300

    restarting the project

--- backend/project/ecommerceapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CheckoutView, ProfileView,IndexView
-
-urlpatterns = [
-    path('', IndexView.as_view(), name='index'), 
-    path('checkout/', CheckoutView.as_view(), name='checkout'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-]
